da706e71af3b11958b05d36b9ce8fe438f043eb9
Xueliang Zhong <xueliang.zhong@linaro.org>
2016-11-16 20:36:35 +0000

    Improve jit code.

--- jit.c
+++ jit.c
@@ -1,16 +1,24 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/mman.h>
 
 #define LOOP 10000000
 
+#define X86_64 1
+
+#ifdef X86_64
 unsigned char code[] = {0xb8, 0x00, 0x00, 0x00, 0x00, 0xc3};
+#endif
+
+void code_copy(unsigned char* to, unsigned char* from, size_t size) {
+  while(size--) {
+    *to++ = *from++;
+  }
+}
 
 int main(void) {
   int i = 0;
   void* mem = mmap(NULL, sizeof(code), PROT_WRITE | PROT_EXEC, MAP_ANON | MAP_PRIVATE, -1, 0);
-  memcpy(mem, code, sizeof(code));
+  code_copy(mem, code, sizeof(code));
 
   int (*func)() = mem;
 
